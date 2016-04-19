cf71a938123d7981540ef141a2874ba2c426f581
Zachary Hinchcliff <zach@MacBook-Pro.local>
2016-04-18 21:19:10 -0700

    added check to make sure word had letter from facebook

--- sticker.py
+++ sticker.py
@@ -4,41 +4,50 @@ from money import Money
 # Sticker Costs
 cost = Money(amount='2.00', currency='USD')
 freshbook = list("facebook")
-
+good_word = True
 facebook = list("facebook")
 userInput = raw_input("Please enter word to see how many stickers you will need: ")
 # Counter for Stickers
 counter = 1
 
+
 def printinfo( word ):
    	# Takes a word makes sure its a string 
    	# turns it to lower case
    	lword = str(word).lower()
-   	print "Word: ", lword
+   	# print "Word: ", lword
    	# Turns Word to Array of single characters
    	warray = list(lword)
-   	print(warray)
+   	# print(warray)
    	# For Loop for each Character in String
    	for x in xrange(0,len(warray)):
-		letter = warray[x]
-		print(letter)
-		if letter in facebook:
-			facebook.remove(letter);
-			print(facebook)
-		# If not in current facebook letters
+   		if warray[x] in freshbook:
+			letter = warray[x]
+			# print(letter)
+			if letter in facebook:
+				facebook.remove(letter);
+				# print(facebook)
+			# If not in current facebook letters
+			else:
+				global counter
+				counter += 1
+				for x in xrange(0,len(freshbook)): 
+					facebook.append(freshbook[x])
+				# print(facebook)
 		else:
-			global counter
-			counter += 1
-			for x in xrange(0,len(freshbook)): 
-				facebook.append(freshbook[x])
-			print(facebook)
-#Print Out
+			global good_word
+			good_word = False;
+			print("Sorry Word Must Contain F, A, C, E, B, O, O, K try again.")
+			return
 printinfo(userInput);
-print("----LETTERS LEFT-----");
-for x in facebook: print x,
-print("");
-print("----STICKER COUNT----");
-print(counter);
-print("----COSTS OF STICKERS----");
-total = counter * cost
-print(total);
+
+if good_word == True:
+	#Print Out
+	print("----LETTERS LEFT-----");
+	for x in facebook: print x,
+	print("");
+	print("----STICKER COUNT----");
+	print(counter);
+	print("----COSTS OF STICKERS----");
+	total = counter * cost
+	print(total);
